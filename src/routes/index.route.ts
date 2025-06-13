73e687acace3971b40a01498b662c310d766ad31
dev-Bambam <ayogood18@gmail.com>
2025-06-13 17:04:17 +0100

    created a route folder in the routes dir, then keep index in the routes dir

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import userRoute from './user.route'
-import adminRoute from './admin.route'
-import authRoute from './auth.route'
+import userRoute from "./route/user.route";
+import adminRoute from "./route/admin.route";
+import authRoute from "./route/auth.route";
 
-const router = Router()
+const router = Router();
 
 router.use("/user", userRoute);
 router.use("/admin", adminRoute);
-router.use('/auth', authRoute)
+router.use("/auth", authRoute);
 export default router;
